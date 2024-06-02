9ddb8ef2bcccd4a63defeabbd1d4279093650e63
tamerabdelsalam <contact@tamerkamal.com>
2024-06-02 18:53:45 +0300

    Added Identiy service

---
+++ projects/Play.Identity/src/Play.Identity.Service/Entities/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+namespace Play.Identity.Service.Entities;
+
+[CollectionName("Roles")]
+public class ApplicationRole : MongoIdentityRole<Guid>
+{
+}
+
--- projects/Play.Identity/src/Play.Identity.Service/Startup.cs
+++ projects/Play.Identity/src/Play.Identity.Service/Startup.cs
@@ -1,16 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using Play.Common.Settings;
+using Play.Identity.Service.Entities;
 
 namespace Play.Identity.Service
 {
@@ -26,6 +25,17 @@ namespace Play.Identity.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            var serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddRoles<ApplicationRole>()
+                .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>
+                (
+                    mongoDbSettings.ConnectionString,
+                    serviceSettings.ServiceName
+                );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -46,6 +56,8 @@ namespace Play.Identity.Service
 
             app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -53,6 +65,7 @@ namespace Play.Identity.Service
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
             });
         }
     }
